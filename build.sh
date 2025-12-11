f3a0c3fb47a87fd8fae09baf9377edf953b300fe
Nagilex <alexander.martinez1851@gmail.com>
2025-12-11 00:50:06 -0300

    Add el diaulo

--- build.sh
+++ build.sh
@@ -7,3 +7,4 @@ poetry install --no-interaction --no-ansi
 # Ejecutar manage.py con poetry run para usar el entorno correcto
 poetry run python manage.py collectstatic --no-input
 poetry run python manage.py migrate --no-input
+poetry run python manage.py cargar_demo
